658ec27eb50f625d6d5fbd165229afc728261cdf
Clint Berry <clint@getweave.com>
2015-02-25 20:39:01 -0700

    Added npm package for ssh deploy

---
+++ web-angular/app/scripts/services/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webAngularApp.Config
+ * @description
+ * # Config
+ * Service in the webAngularApp.
+ */
+angular.module('webAngularApp')
+  .service('Config', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
