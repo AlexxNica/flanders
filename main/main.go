06d7a421b3120e749425d7146f6ff295f19bd42a
Clint Berry <clint@getweave.com>
2015-03-04 22:56:38 -0700

    Made loglevel a command line parameter

--- main/main.go
+++ main/main.go
@@ -11,9 +11,10 @@ import (
 func main() {
 	webport := flag.Int("webport", 8000, "Web server port")
 	sipport := flag.Int("sipport", 9060, "SIP server port")
+	loglevel := flag.String("loglevel", "warn", "Log level")
 	flag.Parse()
 	log.SetLogger(os.Stdout)
-	log.SetLogLevel("debug")
+	log.SetLogLevel(*loglevel)
 
 	go flanders.UDPServer("0.0.0.0", *sipport)
 	flanders.WebServer("0.0.0.0", *webport)
