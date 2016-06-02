4f1875bccc4eb36997349c5dea0a75d32651dd92
Jason Newman <jason@squarepenguin.com>
2016-06-02 14:56:36 -0600

    add flanders run manager

---
+++ flanders.go
@@ -0,0 +1,33 @@
+package flanders
+
+import (
+	"fmt"
+
+	"lab.getweave.com/weave/flanders/db"
+
+	// Choose your db handler or import your own here
+	// _ "lab.getweave.com/weave/flanders/db/influx"
+	_ "lab.getweave.com/weave/flanders/db/mongo"
+)
+
+func Run(sipAddress string, webAddress string, dbAddress string) error {
+
+	err := db.Setup("mongo", dbAddress)
+	if err != nil {
+		return fmt.Errorf("unable to connect to db: %s", err)
+	}
+
+	go h.run()
+
+	err = StartSIPServer(sipAddress)
+	if err != nil {
+		return fmt.Errorf("unable to start sip listener: %s", err)
+	}
+
+	err = StartWebServer(webAddress)
+	if err != nil {
+		return fmt.Errorf("unable to start web server: %s", err)
+	}
+
+	select {}
+}
