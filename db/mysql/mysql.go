0a3c45a1d7eac0c62a2a754ba1677fafddaa2794
Clint Berry <clint@getweave.com>
2016-09-22 00:57:39 -0600

    implemented insert function in mysql db driver

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -5,6 +5,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql" // Loading mysql driver for this database connection
 	"github.com/weave-lab/flanders/db"
+	"github.com/weave-lab/flanders/db/mysql/models"
 )
 
 const (
@@ -26,9 +27,30 @@ func (m *MysqlDB) Connect(connectString string) error {
 	m.connection = connection
 	return err
 }
+
 func (m *MysqlDB) Insert(dbObject *db.DbObject) error {
-	return nil
+	message := &models.Message{}
+	message.Date = dbObject.Datetime
+	message.MicroTS = int64(dbObject.MicroSeconds)
+	message.Method = dbObject.Method
+	message.ReplyReason = dbObject.ReplyReason
+	message.SourceIp = dbObject.SourceIp
+	message.SourcePort = dbObject.SourcePort
+	message.DestinationIp = dbObject.DestinationIp
+	message.DestinationPort = dbObject.DestinationPort
+	message.Callid = dbObject.CallId
+	message.FromUser = dbObject.FromUser
+	message.FromDomain = dbObject.FromDomain
+	message.FromTag = dbObject.FromTag
+	message.ToUser = dbObject.ToUser
+	message.ToDomain = dbObject.ToDomain
+	message.ToTag = dbObject.ToTag
+	message.UserAgent = dbObject.UserAgent
+	message.Cseq = dbObject.Cseq
+
+	return message.Insert(m.connection)
 }
+
 func (m *MysqlDB) Find(filter *db.Filter, options *db.Options, result *db.DbResult) error {
 	return nil
 }
